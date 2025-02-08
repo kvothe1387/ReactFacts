2535bbef3b1b543fe48e5f94920e37bf13b8a4fb
David McCullough <dmccullough488@gmail.com>
2025-02-08 01:09:58 -0600

    Completed Navbar and styling

--- Static Pages practice/src/components/Navbar.jsx
+++ Static Pages practice/src/components/Navbar.jsx
@@ -1,5 +1,10 @@
 export default function Navbar() {
   return (
-    <h1>Navbar component here</h1>
+    <header>
+      <nav>
+        <img src="/src/images/react-logo.png" alt="React Logo" />
+        <span>ReactFacts</span>
+      </nav>
+    </header>
   )
 }
